ab59377533d29389aa1eac5899c2937b0d9195d2
Awiteb <awiteb@pop-os.localdomain>
2021-06-20 03:02:34 +0300

    modified: add name of arguments

--- examples/get_sura_name.py
+++ examples/get_sura_name.py
@@ -2,5 +2,5 @@ from quran_suras import Quran_suras
 
 quran_suras = Quran_suras()
 
-sura_name = quran_suras.get_sura_name(88)
+sura_name = quran_suras.get_sura_name(sura_number=88)
 print(sura_name) # الغاشية
--- examples/get_sura_number.py
+++ examples/get_sura_number.py
@@ -2,5 +2,5 @@ from quran_suras import Quran_suras
 
 quran_suras = Quran_suras()
 
-sura_number = quran_suras.get_sura_number("النمل")
+sura_number = quran_suras.get_sura_number(sura_name="النمل")
 print(sura_number) # 27
